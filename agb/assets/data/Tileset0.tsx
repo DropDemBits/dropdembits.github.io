90860360ef37dd5abe1c5aa508345eae32bc4ca0
DropDemBits <ring0hicc@gmail.com>
2018-08-22 20:21:43 -0400

    First version of AGB out there

---
+++ agb/assets/data/Tileset0.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset0" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="../images/tileatlas0.png" width="64" height="64"/>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="-0.0909091" width="7.72727" height="8.09091"/>
+  </objectgroup>
+ </tile>
+</tileset>
